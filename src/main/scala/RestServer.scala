a94a4e1cf8b93211a3d9aee901696eab95899f7a
Matija Podravec <matija_podravec@fastmail.fm>
2016-07-20 23:45:59 +0100

    Experimenting with actors.

--- src/main/scala/RestServer.scala
+++ src/main/scala/RestServer.scala
@@ -1,33 +1,36 @@
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import scala.io.StdIn
 
 import scala.io.StdIn
 
-object HttpServer {
+object RestServer {
   def main(args: Array[String]) {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
+
+    system.actorOf(Props[actors.RouterActor], "router")
+
     val route =
-      get {
-        pathSingleSlash {
-          complete("Hello!")
-        } ~
-          path("ping") {
-            complete("PONG!")
+      pathPrefix("workflows") {
+        pathEnd {
+          get {
+            complete("list of workflows")
           } ~
-          path("crash") {
-            sys.error("BOOM!")
+          post {
+            complete("create workflow")
           }
+        }
       }
-    // `route` will be implicitly converted to `Flow` using `RouteResult.route2HandlerFlow`
+
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
   }
 }
--- src/main/scala/actors/actors.scala
+++ src/main/scala/actors/actors.scala
@@ -1,6 +1,8 @@
 package actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, Props}
+import akka.routing.ConsistentHashingRouter.ConsistentHashMapping
+import akka.routing._
 import engine._
 
 case class StartWorkflow(wfDef: WorkflowDefinition)
@@ -8,17 +10,39 @@ case class ExecuteRound()
 case class AllWorkflowsFinished()
 case class SomeWorkflowsUpdated()
 
+class RouterActor extends Actor {
+  def hashMapping: ConsistentHashMapping = {
+    case wfId: Int => wfId
+  }
+
+  var router = context.system.actorOf(
+    ConsistentHashingPool(10, hashMapping = hashMapping).props(Props[ViewActor]),
+    name = "view"
+  )
+
+  def receive = {
+    case a: String => router.route(a, sender())
+  }
+}
+
+class ViewActor extends Actor {
+  def receive = ???
+}
+
 class EngineActor extends Actor {
   val engine = new Engine()
 
   def receive = {
     case StartWorkflow(wfDef) =>
       engine.startWorkflow(wfDef)
-      sender ! "Workflow Started"
     case ExecuteRound() =>
-      if (engine.executeRound.nonEmpty)
-        sender ! SomeWorkflowsUpdated()
-      else
-        sender ! AllWorkflowsFinished()
+      engine.executeRound.nonEmpty
   }
+}
+
+object IdGenActor {
+}
+
+class IdGenActor extends Actor {
+  def receive = ???
 }
