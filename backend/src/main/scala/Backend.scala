18ae16c6034e406c170f8327ccdee2d53b087426
Matija Podravec <matija_podravec@fastmail.fm>
2016-09-03 19:19:26 +0100

    Refactored mockup actor.

--- backend/src/main/scala/Backend.scala
+++ backend/src/main/scala/Backend.scala
@@ -1,108 +1,11 @@
-import actors.IdAllocatorActor
-import actors.PrivateActorMessages.{ExecuteRound, IdAllocatorActorRef}
-import akka.actor.{Actor, ActorSystem, Props}
-import common.PublicActorMessages._
-import common.Views._
-import engine.{Engine, IdGenerator}
-import engine.ImplicitConversions._
+import actors.{IdAllocatorActor, MockupActor}
+import actors.PrivateActorMessages.IdAllocatorActorRef
+import akka.actor.{ActorSystem, Props}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Random
-import scala.concurrent.ExecutionContext.Implicits.global
 
-class Mockup extends Actor {
-  val fieldNames = List("Proin", "Etiam", "Integer", "Donec", "Sed", "Aliquam", "Nam", "Vivamus", "Aliquam")
-  val fieldLabels = List(
-    "Proin iaculis nisi eu nisi lobortis",
-    "Etiam cursus libero sed diam volutpat",
-    "Integer eget sem ut libero imperdiet pretium.",
-    "Donec et nisi sed tortor commodo auctor.",
-    "Sed et quam semper, pellentesque ex non, molestie sem.",
-    "Aliquam dapibus tortor eget imperdiet molestie.",
-    "Nam finibus lorem quis suscipit sagittis.",
-    "Vivamus vitae diam sodales, maximus ex quis, placerat leo.",
-    "Aliquam sit amet arcu blandit, posuere sapien at, volutpat neque."
-  )
-  val stringFieldValues = List("Praesent quis mi eu.", "Aliquam blandit faucibus dictum.", "Quisque ut lacinia justo.",
-    "Praesent ultrices scelerisque augue.", "Sed ac dapibus augue.", "Nullam massa erat, bibendum.")
-  val taskStates = List("Open", "Closed")
-  val taskDefNames = List("Interdum", "Pede", "Malesuada", "Amet", "Nunc", "Sit", "Adipiscing", "Curabitur",
-    "Eros", "Eu", "Quam", "Egestas", "Vivamus", "Sed", "Reprehenderit")
-  val workflowStates = List("Waiting on manual task", "Finished", "Running")
-  val workflowNames = List(
-    "Proin iaculis nisi eu nisi lobortis",
-    "Etiam cursus libero sed diam volutpat",
-    "Integer eget sem ut libero imperdiet pretium.",
-    "Donec et nisi sed tortor commodo auctor.",
-    "Sed et quam semper, pellentesque ex non, molestie sem.",
-    "Aliquam dapibus tortor eget imperdiet molestie.",
-    "Nam finibus lorem quis suscipit sagittis."
-  )
-
-  def choice[T](list: List[T]) = list(Random.nextInt(list.length))
-
-  def genManualTaskStringFieldView = {
-    val name = choice(fieldNames)
-    val label = choice(fieldLabels)
-    val value = if (Random.nextBoolean()) Some(choice(stringFieldValues)) else None
-    ManualTaskStringFieldView(name, label, value)
-  }
-
-  def genManualTaskIntFieldView = {
-    val name = choice(fieldNames)
-    val label = choice(fieldLabels)
-    val value = if (Random.nextBoolean()) Some(Random.nextInt) else None
-    ManualTaskIntFieldView(name, label, value)
-  }
-
-  def genManualTaskFieldView = {
-    if (Random.nextBoolean())
-      genManualTaskStringFieldView
-    else
-      genManualTaskIntFieldView
-  }
-
-  def genTaskView = {
-    if (Random.nextBoolean()) {
-      val fields = (1 to Random.nextInt(10)) map (_ => genManualTaskFieldView)
-      ManualTaskView(Random.nextInt(100), choice(taskStates), choice(taskDefNames), fields)
-    } else {
-      TaskView(Random.nextInt(100), choice(taskStates), choice(taskDefNames))
-    }
-  }
-
-  def genWorkflowView = {
-    val tasks = (1 to Random.nextInt(20)) map (_ => genTaskView)
-    WorkflowView(Random.nextInt(100), choice(workflowNames), choice(workflowNames), choice(workflowStates), tasks)
-  }
-
-  val workflows = (1 to 20) map (_ => genWorkflowView)
-
-  implicit val idGen = IdGenerator.SimpleIdGenerator
-
-  val engine = new Engine()
-
-  self ! ExecuteRound
-
-  def receive = {
-    case StartWorkflow(wfDefName, label) =>
-      engine.workflowDefinitions.find(_.name == wfDefName) match {
-        case Some(wfDef) =>
-          val wf = engine.startWorkflow(wfDef, label)
-          sender() ! Workflows(List(wf))
-        case None => sender() ! Error("Invalid workflow definition name %s".format(wfDefName))
-      }
-    case GetWorkflowDefinitions =>
-      sender() ! WorkflowDefinitions(engine.workflowDefinitions map (_.name))
-    case GetWorkflows =>
-      sender() ! Workflows(engine.workflows)
-    case ExecuteRound =>
-      engine.executeRound
-      context.system.scheduler.scheduleOnce(1 second, self, ExecuteRound)
-  }
-}
 
 object Backend extends App {
   val system = ActorSystem("workflows")
@@ -111,7 +14,7 @@ object Backend extends App {
 
   router ! IdAllocatorActorRef(idAllocator)
 
-  //val mockupActor = system.actorOf(Props[Mockup], "mockup")
+  val mockupActor = system.actorOf(Props[MockupActor], "mockup")
 
   Await.result(system.whenTerminated, Duration.Inf)
 }
---
+++ backend/src/main/scala/actors/MockupActor.scala
@@ -0,0 +1,88 @@
+package actors
+
+import akka.actor.Actor
+import common.PublicActorMessages._
+import common.Views._
+import scala.util.Random
+import scala.language.postfixOps
+
+class MockupActor extends Actor {
+  val fieldNames = List("Proin", "Etiam", "Integer", "Donec", "Sed", "Aliquam", "Nam", "Vivamus", "Aliquam")
+  val fieldLabels = List(
+    "Proin iaculis nisi eu nisi lobortis",
+    "Etiam cursus libero sed diam volutpat",
+    "Integer eget sem ut libero imperdiet pretium.",
+    "Donec et nisi sed tortor commodo auctor.",
+    "Sed et quam semper, pellentesque ex non, molestie sem.",
+    "Aliquam dapibus tortor eget imperdiet molestie.",
+    "Nam finibus lorem quis suscipit sagittis.",
+    "Vivamus vitae diam sodales, maximus ex quis, placerat leo.",
+    "Aliquam sit amet arcu blandit, posuere sapien at, volutpat neque."
+  )
+  val stringFieldValues = List("Praesent quis mi eu.", "Aliquam blandit faucibus dictum.", "Quisque ut lacinia justo.",
+    "Praesent ultrices scelerisque augue.", "Sed ac dapibus augue.", "Nullam massa erat, bibendum.")
+  val taskStates = List("Open", "Closed")
+  val taskDefNames = List("Interdum", "Pede", "Malesuada", "Amet", "Nunc", "Sit", "Adipiscing", "Curabitur",
+    "Eros", "Eu", "Quam", "Egestas", "Vivamus", "Sed", "Reprehenderit")
+  val workflowStates = List("Waiting on manual task", "Finished", "Running")
+  val workflowNames = List(
+    "Proin iaculis nisi eu nisi lobortis",
+    "Etiam cursus libero sed diam volutpat",
+    "Integer eget sem ut libero imperdiet pretium.",
+    "Donec et nisi sed tortor commodo auctor.",
+    "Sed et quam semper, pellentesque ex non, molestie sem.",
+    "Aliquam dapibus tortor eget imperdiet molestie.",
+    "Nam finibus lorem quis suscipit sagittis."
+  )
+
+  def choice[T](list: List[T]) = list(Random.nextInt(list.length))
+
+  def genManualTaskStringFieldView = {
+    val name = choice(fieldNames)
+    val label = choice(fieldLabels)
+    val value = if (Random.nextBoolean()) Some(choice(stringFieldValues)) else None
+    ManualTaskStringFieldView(name, label, value)
+  }
+
+  def genManualTaskIntFieldView = {
+    val name = choice(fieldNames)
+    val label = choice(fieldLabels)
+    val value = if (Random.nextBoolean()) Some(Random.nextInt) else None
+    ManualTaskIntFieldView(name, label, value)
+  }
+
+  def genManualTaskFieldView = {
+    if (Random.nextBoolean())
+      genManualTaskStringFieldView
+    else
+      genManualTaskIntFieldView
+  }
+
+  def genTaskView = {
+    if (Random.nextBoolean()) {
+      val fields = (1 to Random.nextInt(10)) map (_ => genManualTaskFieldView)
+      ManualTaskView(Random.nextInt(100), choice(taskStates), choice(taskDefNames), fields)
+    } else {
+      TaskView(Random.nextInt(100), choice(taskStates), choice(taskDefNames))
+    }
+  }
+
+  def genWorkflowView = {
+    val tasks = (1 to Random.nextInt(20)) map (_ => genTaskView)
+    WorkflowView(Random.nextInt(100), choice(workflowNames), choice(workflowNames), choice(workflowStates), tasks)
+  }
+
+  val workflows = (1 to 20) map (_ => genWorkflowView)
+
+  def receive = {
+    case StartWorkflow(wfDefName, label) =>
+      if (Random.nextBoolean())
+        sender() ! Workflows(List(genWorkflowView))
+      else
+        sender() ! Error("Cannot start a workflow!")
+    case GetWorkflowDefinitions =>
+      sender() ! WorkflowDefinitions((1 to 5) map (_ => choice(workflowNames)))
+    case GetWorkflows =>
+      sender() ! Workflows(workflows)
+  }
+}
