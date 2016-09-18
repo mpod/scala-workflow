1bf72dc44d67a070e6410435ff7761b67ce2cdbe
Matija Podravec <matija_podravec@fastmail.fm>
2016-09-18 16:20:00 +0100

    Added demo example.

--- backend/src/main/scala/definitions/Demo.scala
+++ backend/src/main/scala/definitions/Demo.scala
@@ -6,27 +6,27 @@ import engine.TaskDefinition._
 import engine.{ActionResult, ManualTaskDefinition, TaskDefinition, WorkflowDefinition}
 
 object Demo extends WorkflowDefinition {
-  val man1 = new ManualTaskDefinition(List(
+  val manual1 = new ManualTaskDefinition(List(
     IntField("Simple Int field", "intfield")
   ))
-  val man2 = new ManualTaskDefinition(List(
+  val manual2 = new ManualTaskDefinition(List(
     StringField("Simple String field", "strfield")
   ))
   val branch = new BranchTaskDefinition(context => {
     context.task.parent.get.value.get[Int]("intfield").get > 100
   })
   val subflow = new SubWorkflowTaskDefinition(Demo)
-  val join1 = new JoinTaskDefinition(Set(subflow, branch), true)
-  val join2 = new JoinTaskDefinition(Set(join1, man2))
+  val join1 = new JoinTaskDefinition(Set(subflow, branch), waitOnlyForFirst = true)
+  val join2 = new JoinTaskDefinition(Set(join1, manual2))
 
   override val transitions: Map[(TaskDefinition, ActionResult), List[TaskDefinition]] = Map(
-    (StartTaskDefinition, Ok) -> List(man1, man2),
-    (man1, Ok) -> List(branch),
+    (StartTaskDefinition, Ok) -> List(manual1, manual2),
+    (manual1, Ok) -> List(branch),
     (branch, Yes) -> List(subflow),
     (branch, No) -> List(join1),
     (subflow, Ok) -> List(join1),
     (join1, Ok) -> List(join2),
-    (man2, Ok) -> List(join2),
+    (manual2, Ok) -> List(join2),
     (join2, Ok) -> List(EndTaskDefinition)
   )
   override val name: String = "Demo"
